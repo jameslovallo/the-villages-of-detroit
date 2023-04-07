7db4f7a94fce2da5b0c72dfdf4131468b425c635
James Lovallo <patrickonparker@gmail.com>
2023-04-06 22:45:44 -0400

    .

--- @/components/app-nav.js
+++ @/components/app-nav.js
@@ -7,7 +7,7 @@ ardi({
     return html`
       <nav>
         <ardi-dialog drawer ref="drawer">
-          <button slot="opener">
+          <button slot="opener" aria-label="Open primary navigation.">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
               <path d="M3,6H21V8H3V6M3,11H21V13H3V11M3,16H21V18H3V16Z" />
             </svg>
