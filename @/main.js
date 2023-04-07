5e056fabed283664bcca608c1ff7202324c35302
James Lovallo <patrickonparker@gmail.com>
2023-04-06 22:43:39 -0400

    remove markdown

--- @/main.js
+++ @/main.js
@@ -4,7 +4,6 @@ const globals = [
   'app-layout',
   'app-nav',
   'list-item',
-  'mark-down',
   'spa-link',
   'spa-root',
 ]
@@ -14,7 +13,7 @@ import('//unpkg.com/ardi@latest/demos/dialog/dialog.js')
 import('https://unpkg.com/ardi/demos/youtube/youtube.js')
 
 // fade in gracefully when components are loaded
-const all = ['ardi-dialog', ...globals]
+const all = ['ardi-dialog', 'ardi-youtube', ...globals]
 const isDefined = all.map((c) => customElements.whenDefined(c))
 await Promise.allSettled(isDefined)
 document.body.style.opacity = 1
