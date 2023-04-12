9b6bce1e9196950f53454499392a4be981d0cf87
James Lovallo <patrickonparker@gmail.com>
2023-04-11 22:30:44 -0500

    refactor(main.js): remove 'ardi-youtube' from the list of components to load before fading in the page

--- @/main.js
+++ @/main.js
@@ -13,7 +13,7 @@ globals.forEach((c) => import(`/@/components/${c}.js`))
 import('//unpkg.com/ardi@latest/demos/dialog/dialog.js')
 
 // fade in gracefully when components are loaded
-const all = ['ardi-dialog', 'ardi-youtube', ...globals]
+const all = ['ardi-dialog', ...globals]
 const isDefined = all.map((c) => customElements.whenDefined(c))
 await Promise.allSettled(isDefined)
 document.body.style.opacity = 1
