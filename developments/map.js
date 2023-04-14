4944e5f238a54131f374688dabd1c2e5465de152
James Lovallo <patrickonparker@gmail.com>
2023-04-14 12:29:17 -0400

    .

--- developments/map.js
+++ developments/map.js
@@ -223,4 +223,4 @@ tag.setAttribute(
   'src',
   'https://maps.googleapis.com/maps/api/js?key=AIzaSyC0mhVPdzcRZkxDIYuokc593p3h2U0nfVg&callback=initMap&libraries=marker&v=beta'
 )
-document.body.appendChild('tag')
+document.body.appendChild(tag)
