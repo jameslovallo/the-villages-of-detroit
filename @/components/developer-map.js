742a0bc99c3b1b7fe1bdfff64392e28640d0edd4
James Lovallo <patrickonparker@gmail.com>
2023-04-24 13:07:21 -0400

    update survey handling

--- @/components/developer-map.js
+++ @/components/developer-map.js
@@ -7,10 +7,12 @@ import {
 import developments from '/@/assets/developments/developments.js'
 
 window.addEventListener('load', () => {
+  // create map
   const devMap = map(document.querySelector('#map')).setView(
     [42.361582463007856, -82.99748471510723],
     14
   )
+  // create tiles
   tileLayer(
     'https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png',
     {
@@ -20,42 +22,73 @@ window.addEventListener('load', () => {
       maxZoom: 20,
     }
   ).addTo(devMap)
-
-  developments.forEach((d) => {
-    const {
-      title,
-      color,
-      position: { lat, lng },
-      response,
-    } = d
-
-    const pin = marker([lat, lng], {
-      icon: icon({
-        iconUrl: `/@/assets/developments/${color}.svg`,
-        iconSize: [32, 32],
-        iconAnchor: [16, 30],
-        popupAnchor: [0, -30],
-      }),
-    }).addTo(devMap)
-
-    if (color === 'green') {
-      const dialog = document.createElement('dialog')
-      document.body.appendChild(dialog)
-      dialog.classList.add('development-modal')
-      dialog.innerHTML = `
-          <h2>${title}</h2>
-          <div class="development-modal-content">
-            <developer-survey property="${response}"></developer-survey>
-          </div>
-          <form method="dialog">
-            <button>Close</button>
-          </form>
-        `
-      pin.addEventListener('click', () => {
-        dialog.showModal()
+  // get data from Google sheet
+  fetch(
+    'https://sheets.googleapis.com/v4/spreadsheets/1A09bCVNcygBPIGPVXMqQdS87BII54Rtw9Dd4Rm9weOI/values/A1:AZ100?key=AIzaSyCSD5oD-d2ORbqI6375p_CnRhdrHXEXM14'
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      const { values } = data
+      const questions = values[0]
+      // loop over developments
+      developments.forEach(async (d) => {
+        // define data
+        const {
+          title,
+          color,
+          position: { lat, lng },
+          response,
+        } = d
+        // create pin
+        const pin = marker([lat, lng], {
+          icon: icon({
+            iconUrl: `/@/assets/developments/${color}.svg`,
+            iconSize: [32, 32],
+            iconAnchor: [16, 30],
+            popupAnchor: [0, -30],
+          }),
+        }).addTo(devMap)
+        // handle survey responses (green pins)
+        if (color === 'green') {
+          // create dialog
+          const dialog = document.createElement('dialog')
+          document.body.appendChild(dialog)
+          dialog.classList.add('development-modal')
+          // build survey markup
+          let surveyMarkup = /* html */ `<h3>Developer Survey Response</h3>`
+          for (let i = 1; i < questions.length; i++) {
+            const question = questions[i].trim()
+            const punc =
+              question.endsWith('?') || question.endsWith('.') ? '' : '?'
+            surveyMarkup += /* html */ `
+              <p class="question">
+                <span class="bubble">${question + punc}</span>
+              </p>
+              <p class="answer">
+                <span class="bubble">
+                ${values[response][i] || 'No reponse'}
+                </span>
+              </p>
+            `
+          }
+          // set dialog markup
+          dialog.innerHTML = /* html */ `
+            <h2>${title}</h2>
+            <div class="development-modal-content">
+              ${surveyMarkup}
+            </div>
+            <form method="dialog">
+              <button>Close</button>
+            </form>
+          `
+          // show dialog on pin click
+          pin.addEventListener('click', () => {
+            dialog.showModal()
+          })
+          // handle red and gray pins
+        } else {
+          pin.bindPopup(`<h2>${title}</h2>`)
+        }
       })
-    } else {
-      pin.bindPopup(`<h2>${title}</h2>`)
-    }
-  })
+    })
 })
