7b4a4331237c0d0a3344dd39ab7380751fb060b8
James Lovallo <patrickonparker@gmail.com>
2023-04-30 15:34:44 -0400

    test using self-hosted layout components again

--- @/main.js
+++ @/main.js
@@ -3,7 +3,14 @@ import 'https://unpkg.com/ardi/@/components/app-root.js'
 import 'https://unpkg.com/ardi/demos/dialog/dialog.js'
 
 // load global components
-const globals = ['app-footer', 'app-layout', 'app-nav', 'list-item']
+const globals = [
+  'app-root',
+  'app-link',
+  'app-layout',
+  'app-nav',
+  'app-footer',
+  'list-item',
+]
 globals.forEach((c) => import(`/@/components/${c}.js`))
 
 // fade in gracefully when components are loaded
